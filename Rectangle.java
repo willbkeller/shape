e9282b0e9fb7fab8c1909b9cb309642ce6bc9c49
William Keller <williamkeller@Williams-MacBook-Pro.local>
2017-03-01 15:00:46 -0600

    implementing methods and Driver

--- Rectangle.java
+++ Rectangle.java
@@ -1,3 +1,30 @@
 public class Rectangle extends Shape{
 
+   private double height;
+   private double base;
+
+   private Rectangle(){
+      this.height = 0;
+      this.base = 0;
+   }
+
+   public Rectangle(double height, double base){
+      this.height = height;
+      this.base = base;
+   }
+
+   public double getArea(){
+      double area = this.base * this.height;
+      return area;
+   }
+
+   public double getPerimeter(){
+      double perimeter = (2*this.base) + (2*this.height);
+      return perimeter;
+   }
+
+   public String toString(){
+      return "Rectangle:\nArea = " + getArea() + "\nPerimeter = " + getPerimeter();
+   }
+
 }
--- Square.java
+++ Square.java
@@ -1,3 +1,14 @@
 public class Square extends Rectangle{
 
+   private Square(){
+      super(0, 0);
+   }
+
+   public Square(double side){
+      super(side, side);
+   }
+
+   public String toString(){
+      return "Square:\nArea = " + getArea() + "\nPerimeter = " + getPerimeter();
+   }
 }
